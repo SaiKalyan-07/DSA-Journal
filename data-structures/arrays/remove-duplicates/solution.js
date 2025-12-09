17fabc59be22a4d55fe6b4af1655e5cdfd2e1399
SaiKalyan-07 <skravikumar2004@gmail.com>
2025-12-09 17:54:36 +0530

    feat: add solutions for palindrome number, reverse integer, remove duplicates, and remove element

---
+++ data-structures/arrays/remove-duplicates/solution.js
@@ -0,0 +1,14 @@
+var removeDuplicates = function(nums) {
+    let x = 0; 
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > nums[x]) {
+            x++;         
+            nums[x] = nums[i];
+        }
+    }
+    return x + 1; 
+};
+
+let result = removeDuplicates([0,0,1,1,1,2,2,3,3,4]);
+console.log(result);
+
